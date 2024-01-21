f224db5ddbe98212777c79f1afd3052e323c0544
Vincentius Christian Chandra <crist.vincent10@gmail.com>
2024-01-21 16:25:42 +0700

    add view empty and error, recyclerview

--- app/src/main/java/com/example/pixelplax/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/example/pixelplax/ui/movie/MovieFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pixelplax.databinding.FragmentMovieBinding
@@ -23,13 +22,8 @@ class MovieFragment : Fragment() {
             ViewModelProvider(this)[MovieViewModel::class.java]
 
         _binding = FragmentMovieBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        movieViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
--- app/src/main/java/com/example/pixelplax/ui/series/SeriesFragment.kt
+++ app/src/main/java/com/example/pixelplax/ui/series/SeriesFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.pixelplax.databinding.FragmentSeriesBinding
@@ -23,13 +22,8 @@ class SeriesFragment : Fragment() {
             ViewModelProvider(this)[SeriesViewModel::class.java]
 
         _binding = FragmentSeriesBinding.inflate(inflater, container, false)
-        val root: View = binding.root
 
-        val textView: TextView = binding.textDashboard
-        seriesViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
-        return root
+        return binding.root
     }
 
     override fun onDestroyView() {
